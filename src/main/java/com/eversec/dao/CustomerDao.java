70fd783d3200e1db496eb810b77356e526bd7042
coderChenMing <280433706@qq.com>
2021-06-20 17:29:16 +0800

    jpql测试

--- src/main/java/com/eversec/dao/CustomerDao.java
+++ src/main/java/com/eversec/dao/CustomerDao.java
@@ -3,18 +3,29 @@ package com.eversec.dao;
 import com.eversec.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 /*
-* dao的实现：定义dao接口，继承JpaRepository,JpaSpecificationExecutor
-* JpaRepository<操作的实体类型，实体类中主键属性类型>:封装了crud操作
-* JpaSpecificationExecutor<操作的实体类型>：封装了复杂操作
-* */
-public interface CustomerDao extends JpaRepository<Customer,Long>, JpaSpecificationExecutor<Customer> {
+ * dao的实现：定义dao接口，继承JpaRepository,JpaSpecificationExecutor
+ * JpaRepository<操作的实体类型，实体类中主键属性类型>:封装了crud操作
+ * JpaSpecificationExecutor<操作的实体类型>：封装了复杂操作
+ * */
+public interface CustomerDao extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
     /*复杂查询
-    * 1.定义方法
-    * 2.加query注解标注sql
-    * */
-    @Query(value="from Customer  where custName = ?")
+     * 1.定义方法
+     * 2.加query注解标注sql
+     * */
+    @Query(value = "from Customer  where custName = ?")
     public Customer findByName(String custNamer);
+
+    @Query(value = "from Customer  where custName = ?1 and custId= ?2")
+    public Customer findByNameAndId(String name, long id);
+
+    @Query(value = "from Customer  where custName = ?2 and custId= ?1")
+    public Customer findByNameAndId(long id, String name);
+
+    @Query(value = "update Customer  set custName = ?2   where  custId= ?1")
+    @Modifying
+    public int updateNameById(long id, String name);
 }
--- src/test/java/com/eversec/test/JpaSqlTest.java
+++ src/test/java/com/eversec/test/JpaSqlTest.java
@@ -5,17 +5,41 @@ import com.eversec.domain.Customer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
 public class JpaSqlTest {
     @Autowired
     private CustomerDao customerDao;
+
     @Test
     public void selectByName() {
         Customer customer = customerDao.findByName("林平之");
         System.out.println(customer);
     }
+
+    @Test
+    public void selectByNameAndId() {
+        //Customer customer = customerDao.findByNameAndId("林平之",8l);
+        Customer customer = customerDao.findByNameAndId(8l,"林平之");
+        System.out.println(customer);
+    }
+    /*Transactional:自定义的更新和删除必须添加
+    springdatajpa 完成更新和删除需要手动添加事务，且其默认自动回滚事务
+    * */
+    @Test
+    @Transactional
+    @Rollback(value=false)/*设置不回滚*/
+    public void updateNameById() {
+        //Customer customer = customerDao.findByNameAndId("林平之",8l);
+        int count = customerDao.updateNameById(9l, "岳不群");
+        System.out.println(count);
+        Customer customer = customerDao.findByNameAndId(9l, "岳不群");
+        System.out.println(customer);
+    }
+
 }
